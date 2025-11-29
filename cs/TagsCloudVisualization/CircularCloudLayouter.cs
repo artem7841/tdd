bf681278e45b1cb1ffb4a3d4852e7fd125d7cfa0
Artem <17artem12@gmail.com>
2025-11-29 17:12:07 +0500

    Написана реализация

--- cs/TagsCloudVisualization/CircularCloudLayouter.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -6,16 +6,135 @@ class CircularCloudLayouter
 {
     private readonly Point center;
     private readonly List<Rectangle> rectangles;
+    private int aCoef = 1;
 
     public CircularCloudLayouter(Point center)
     {
         this.center = center;
         this.rectangles = new List<Rectangle>();
     }
+
+    private Point GetLeftTopCornerFromCenter(Point center, Size size)
+    {
+        int X = center.X -  size.Width / 2;
+        int Y = center.Y -  size.Height / 2;
+        Point result = new Point(X, Y);
+        return result;
+    }
     
+    private Point GetCenterOfRectangle(Rectangle rectangle)
+    {
+        int X = rectangle.X + rectangle.Width / 2;
+        int Y = rectangle.Y + rectangle.Height / 2;
+        Point result = new Point(X, Y);
+        return result;
+    }
+
+    public bool IsIntersectingWithOtherRectangles(Rectangle rectangle)
+    {
+
+        foreach (Rectangle otherRectangle in rectangles)
+        {
+            if (otherRectangle.IntersectsWith(rectangle))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Rectangle PushRectangleToCenter(Rectangle rec)
+    {
+
+        Rectangle rectangle = rec;
+        int prevX = rectangle.X;
+        int prevY = rectangle.Y;
+        while (GetCenterOfRectangle(rectangle).X != center.X)
+        {
+            
+            prevX = rectangle.X;
+            if (center.X - GetCenterOfRectangle(rectangle).X > 0)
+            {
+                rectangle.X++;
+            }
+            else
+            {
+                rectangle.X--;
+            }
+            
+            if (IsIntersectingWithOtherRectangles(rectangle))
+            {
+                rectangle.Y = prevY;
+                rectangle.X = prevX;
+                break;
+            }
+        }
+        while (GetCenterOfRectangle(rectangle).Y != center.Y)
+        {
+            prevY = rectangle.Y;
+            if (center.Y - GetCenterOfRectangle(rectangle).Y > 0)
+            {
+                rectangle.Y++;
+            }
+            else
+            {
+                rectangle.Y--;
+            }
+            
+            if (IsIntersectingWithOtherRectangles(rectangle))
+            {
+                rectangle.Y = prevY;
+                rectangle.X = prevX;
+                break;
+            }
+
+        }
+
+        return rectangle;
+    }
+
+    private Point GetNextPointOnSpiral(double angle)
+    {
+        double angleInRadians = Math.PI / 180 * angle;
+        double cosine = Math.Cos(angleInRadians);
+        double sine = Math.Sin(angleInRadians);
+
+        int x =  (int)(aCoef * angleInRadians * cosine + center.X);
+        int y =  (int)(aCoef * angleInRadians * sine +  center.Y);
+        
+        return new Point(x, y);
+    }
 
     public Rectangle PutNextRectangle(Size rectangleSize)
     {
-        throw new NotImplementedException();
+        if (rectangleSize.Width <= 0 || rectangleSize.Height <= 0)
+        {
+            throw new ArgumentOutOfRangeException("rectangleSize is <= 0");
+        }
+        
+        Point leftTopCorner = GetLeftTopCornerFromCenter(center, rectangleSize);
+        Rectangle rectangle = new Rectangle(leftTopCorner, rectangleSize);
+        
+        if (rectangles.Count == 0)
+        {
+            rectangles.Add(rectangle);
+            return rectangle;
+        }
+        
+        double angle = 180;
+        while (IsIntersectingWithOtherRectangles(rectangle))
+        {
+            angle++;
+            Point nextPointOnSpiral = GetNextPointOnSpiral(angle);
+            Point newRecPosition = GetLeftTopCornerFromCenter(nextPointOnSpiral,  rectangleSize);
+            rectangle.X = newRecPosition.X;
+            rectangle.Y = newRecPosition.Y;
+        }
+
+        Rectangle result = PushRectangleToCenter(rectangle);
+
+        
+        rectangles.Add(result);
+        return result;
     }
 }
